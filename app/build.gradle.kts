b411db72049d4bc0aab8089dec943c9f23aeb342
Wxjxpp <wxjxpp@users.noreply.github.com>
2026-08-22 04:12:18 +0000

    chore: 版本 0.4.3(8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.wxjxpp.neiro"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.4.2"
+        versionCode = 8
+        versionName = "0.4.3"
     }
 
     buildTypes {
